ba22b4f714bc22d1858ad4c323cef061231b5a96
Ibrahim Nour <ibrahamnour22@gmail.com>
2021-02-23 11:25:57 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ document.getElementById("create-task-form").addEventListener('submit',function(e){
+  let toDoList = document.getElementById("new-task-description").value;
+  let ul=document.getElementById("list").createElement('ul');
+     ul.innerHTML += `<li> ${toDoList}</li>`;
+     document.body.appendChild(ul);
+     document.getElementById("new-task-description").value='';
+     e.preventDefault();
+
+});
+
 });
